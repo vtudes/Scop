1fad8861de746c26591c88d10fee14859eabd651
Hugo Bouillot <hbouillo@student.42.fr>
2020-08-27 20:23:47 +0200

    Woops

--- srcs/main.c
+++ srcs/main.c
@@ -23,28 +23,29 @@ int		        main(int ac, char **av)
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 	
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
     glUseProgram(env.shaders.program);
 
     while (!glfwWindowShouldClose((&env)->win.ptr))
     {
-		glClear( GL_COLOR_BUFFER_BIT );
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         key_controls(&env);
 		
 		// BEGIN DRAWING
-		
+
+    	glEnableVertexAttribArray(0);
+
+	    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
 		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-		
+
+    	glDisableVertexAttribArray(0);
+
 		// END DRAWING
     	
 		glfwSwapBuffers(env.win.ptr);
     	glfwPollEvents();
     }
-    glDisableVertexAttribArray(0);
     glfwTerminate();
 	return (0);
 }
